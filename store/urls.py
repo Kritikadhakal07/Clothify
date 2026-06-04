67d00396455c5f61064954f59e71d076abf119c3
kritikadhakal07 <kritikadhakal211@gmail.com>
2026-06-04 14:19:52 +0545

    added product and category

--- store/urls.py
+++ store/urls.py
@@ -7,4 +7,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     
     path('',views.store,name='store'),
+    path('<slug:category_slug>/',views.store,name='product_by_category'),
 ]
